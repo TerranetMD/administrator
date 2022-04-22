fa40c9155efc812ed2e4b5f2dd94fc070519b403
vitalyrotari <cky.adc@gmail.com>
2022-04-22 19:22:22 +0300

    extend File field object

--- src/Field/File.php
+++ src/Field/File.php
@@ -6,6 +6,20 @@ use Terranet\Administrator\Scaffolding;
 
 class File extends Field
 {
+    /**
+     * Accepted file types
+     *
+     * @var string[]
+     */
+    public array $fileTypes = [];
+
+    public bool $multiple = false;
+
+    /**
+     * Max file size in kilobytes
+     */
+    public int $maxFileSize = 1024;
+
     public $visibility = [
         Scaffolding::PAGE_INDEX => true,
         Scaffolding::PAGE_EDIT => true,
@@ -27,5 +41,53 @@ class File extends Field
         return [
             'attachment' => $this->model ? $this->model->{$this->id} : null,
         ];
+    }
+
+    /**
+     * @param string[] $fileTypes
+     */
+    public function setFileTypes(array $fileTypes): self
+    {
+        $this->fileTypes = $fileTypes;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function fileTypes(): array
+    {
+        return $this->fileTypes;
+    }
+
+    public function setMultiple(bool $multiple): self
+    {
+        $this->multiple = $multiple;
+
+        return $this;
+    }
+
+    public function multiple(): bool
+    {
+        return $this->multiple;
+    }
+
+    /**
+     * @param int $fileSize Max file size in kilobytes
+     */
+    public function setMaxFileSize(int $fileSize): self
+    {
+        $this->maxFileSize = $fileSize;
+
+        return $this;
+    }
+
+    /**
+     * @return int Max file size in kilobytes
+     */
+    public function maxFileSize(): int
+    {
+        return $this->maxFileSize;
     }
 }
