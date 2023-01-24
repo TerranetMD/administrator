060283ddb9980adb3a45f5757f0b07023afdaa02
Vitalie Rotari <cky.adc@gmail.com>
2023-01-24 19:59:34 +0200

    send render data to custom formatter callback

--- src/Field/Field.php
+++ src/Field/Field.php
@@ -319,12 +319,12 @@ abstract class Field implements Sortable, AutoTranslatable
             'attributes' => $this->getAttributes(),
         ];
 
-        if ($this->hasCustomFormat()) {
-            return $this->callFormatter($this->model, $page);
-        }
-
         if (method_exists($this, $dataGetter = 'on' . Str::title($page))) {
             $data += call_user_func([$this, $dataGetter]);
+        }
+
+        if ($this->hasCustomFormat()) {
+            return $this->callFormatter($this->model, $page, $data);
         }
 
         if (View::exists($view = $this->template($page))) {
